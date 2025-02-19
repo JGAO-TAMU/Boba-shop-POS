a0345bfbef2e583e550c232e39faa67dd32599bf
Zak-2005 <zak.mob2005@gmail.com>
2025-02-19 14:20:47 -0600

    fixed query

--- queries/weekly_sales_history.sql
+++ queries/weekly_sales_history.sql
@@ -1,27 +1,10 @@
 --SPECIAL QUERY #1
 
--- custom params:
--- \set year xxxx
--- \set week x
--- \i weekly_sales_history.sql
-
--- shows total sales for specified week
---default values
-\if :{?year}
-\else
-\set year 2025
-\endif
-
-\if :{?week}
-\else
-\set week 1
-\endif
+-- shows total sales for all weeks
 
 SELECT 
     DATE_TRUNC('week', timestamp) AS week_start,
     COUNT(*) AS order_count
 FROM orders
-WHERE EXTRACT(ISOYEAR FROM timestamp) = :year
-AND EXTRACT(WEEK FROM timestamp) = :week
 GROUP BY week_start
 ORDER BY week_start;
