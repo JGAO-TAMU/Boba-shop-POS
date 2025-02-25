235bcb2482d5a3dff7a8db7f5210bdb31cf0ee62
Nexoloft <justin.nguyen921@gmail.com>
2025-02-25 17:42:04 -0600

    Added back README and fixed powershell compile and run file

--- WindowsCompileRun.ps1
+++ WindowsCompileRun.ps1
@@ -1,7 +1,7 @@
 #compile
 
-javac -cp "postgresql-42.2.8.jar" -d bin (Get-ChildItem -Path GUI/bobaapp -Recurse -Filter "*.java" | ForEach-Object { $_.FullName })
+javac -cp "GUI/postgresql-42.2.8.jar" -d GUI/bin (Get-ChildItem -Path GUI/bobaapp -Recurse -Filter "*.java" | ForEach-Object { $_.FullName })
 
 #run
 
-java -cp "bin;postgresql-42.2.8.jar" bobaapp.Main
+java -cp "GUI/bin;postgresql-42.2.8.jar" bobaapp.Main
