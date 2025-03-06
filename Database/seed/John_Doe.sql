1834d3fa1bd00b2d83d6b839c2743e9c032f254d
JGAO-TAMU <156945517+JGAO-TAMU@users.noreply.github.com>
2025-03-06 16:23:36 -0600

    add manager to database seed

--- Database/seed/John_Doe.sql
+++ Database/seed/John_Doe.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 INSERT INTO Employee (name, accessLevel, clockIn, clockOut)
-VALUES ('John Doe', 1, NOW(), NULL);
-
+VALUES ('John Doe', 1, NOW(), NULL),
+VALUES ('Joe Schmo', 0, NOW(), NULL);
 COMMIT;
